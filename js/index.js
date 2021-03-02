062c37fdf4c0a7844e9a41caa47946f99c4db71f
Felipe de Lima Lopes <felipe.lopes@pampa.compasso>
2021-03-02 13:21:41 -0300

    Atividades JavaScript

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var str = 'oi'
+var numb = 25
+var isBoolean = true
+var matrix = []
+var objt = {}
+var func = () => {}
+
+
+console.log(typeof str)
+console.log(typeof numb)
+console.log(typeof isBoolean)
+console.log(typeof matrix)
+console.log(typeof objt)
+console.log(typeof func)
