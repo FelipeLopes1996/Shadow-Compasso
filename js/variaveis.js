2e3f6f38be0ad8514ef2464256dc3371913a97b7
Felipe de Lima Lopes <felipe.lopes@pampa.compasso>
2021-03-02 13:52:54 -0300

    modificando nome do arquivo para variáveis.js

---
+++ js/variaveis.js
@@ -0,0 +1,14 @@
+var str = 'oi' // tipo string
+var numb = 25 // tipo number
+var isBoolean = true // tipo boolean
+var matrix = [] // array também é um tipo object
+var objt = {} // tipo Object
+var func = () => {} // tipo variável função(functiom)
+
+
+console.log(typeof str)
+console.log(typeof numb)
+console.log(typeof isBoolean)
+console.log(typeof matrix)
+console.log(typeof objt)
+console.log(typeof func)
